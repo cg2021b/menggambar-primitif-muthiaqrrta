eff1953a1b82892269e32358ff5669033fddeceb
muthiaqrrta <68548653+muthiaqrrta@users.noreply.github.com>
2021-09-16 05:41:38 +0700

    Update Menggambar Primititf

--- Menggambar Primitif/main.js
+++ Menggambar Primitif/main.js
@@ -2,7 +2,7 @@ function main(){
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
-    //koordinat titik segitiga--> butuh 6 titik
+    // koordinat titik segitiga --> butuh 6 titik
     var vertices = [
         -0.5, 0.5,   //Titik A
         -0.5, -0.5,  //Titik B
@@ -13,14 +13,14 @@ function main(){
     ];
 
     var positionBuffer = gl.createBuffer();
-    //create buffer : banyak titik
-    //bind buffer untuk gabungin banyak titik
+    // create buffer : jumlah titik
+    // bind buffer untuk menggabungkan banyak titik
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     //buffer data : copy data
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
      
-    //definisi vertex
+    // Mendefinisikan vertex
     var vertexShaderCode = `
     attribute vec2 a_Position;
     void main(){
@@ -30,37 +30,37 @@ function main(){
 
     //var vertexShaderCode = document.getElementById("vertexShaderCode").text;
 
-     //membuat vertex shader
+     // Membuat vertex shader
      var vertexShader = gl.createShader(gl.VERTEX_SHADER);
      gl.shaderSource(vertexShader, vertexShaderCode);
      gl.compileShader(vertexShader);
 
-     //definisi fragment shader dari html
+     // Mendefinisikan fragment shader dari html
     var fragmentShaderCode = document.getElementById("fragmentShaderCode").text;
     
-    //membuat fragment shader
+    // Membuat fragment shader
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
     gl.compileShader(fragmentShader);
 
-    //package program --> compile
+    // Package program --> compile
     var shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    //untuk menggambar 3 titik x, y vertex
+    // untuk menggambar 3 titik x, y vertex
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
     gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aPosition);
 
-    //set warna background
+    // set warna background
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
-    //clear background
+    // clear background
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    //instruksi untuk menggambar
+    // instruksi untuk menggambar
     gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
